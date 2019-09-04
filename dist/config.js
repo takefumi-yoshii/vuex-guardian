c8ce6424eeda11c90606bd3e030b2f807cf7686d
takefumi-yoshii <takefumi.yoshii@dena.jp>
2019-09-04 19:05:22 +0900

    update

--- dist/config.js
+++ dist/config.js
@@ -44,9 +44,9 @@ var modulesTypesConstants = {
 };
 //_______________________________________________________
 //
-exports.baseDir = '../vuex-guardian-example';
-exports.storeDir = '../vuex-guardian-example/store';
-exports.distDir = '../vuex-guardian-example/types/vuex';
+exports.baseDir = '.';
+exports.storeDir = './store';
+exports.distDir = './types/vuex';
 exports.constants = __assign({}, dependenciesConstants, tsUtilityTypesConstants, shimsTypesConstants, modulesTypesConstants);
 exports.config = {
     baseDir: exports.baseDir,
