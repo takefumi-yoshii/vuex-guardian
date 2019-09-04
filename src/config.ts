b11825145134affbdd3e10f7ee8efb31c259acba
takefumi-yoshii <takefumi.yoshii@dena.jp>
2019-09-04 19:04:48 +0900

    fix default value

--- src/config.ts
+++ src/config.ts
@@ -31,9 +31,9 @@ const modulesTypesConstants = {
 }
 //_______________________________________________________
 //
-export const baseDir = '../vuex-guardian-example'
-export const storeDir = '../vuex-guardian-example/store'
-export const distDir = '../vuex-guardian-example/types/vuex'
+export const baseDir = '.'
+export const storeDir = './store'
+export const distDir = './types/vuex'
 export const constants = {
   ...dependenciesConstants,
   ...tsUtilityTypesConstants,
